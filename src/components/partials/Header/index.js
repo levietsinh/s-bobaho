fae0b7cd83114063cdd2b2a1c553164a5c4ac0a4
Sinh Le <levietsinh201194@gmail.com>
2022-03-26 23:43:09 +0700

    Event scroll header

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Link } from 'react-router-dom'
 import styles from './header.module.scss'
 import Logo from './../../../images/logo.png'
 import Cart from './../../../images/cart.png'
 const Header = () => {
+  const [isScroll, setIsScroll] = useState(false)
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.pageYOffset === 0 ? setIsScroll(false) : setIsScroll(true)
+    })
+    return () => {
+      window.removeEventListener('scroll', () => {})
+    }
+  })
   return (
     // <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <header className={styles.header}>
+    <header className={isScroll ? styles.headerScroll : styles.header}>
       <div className={styles.header__logo}>
         <Link to={process.env.PUBLIC_URL}>
           <img src={Logo}/>
